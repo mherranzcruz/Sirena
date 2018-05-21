22c93f159322f68e90a74a09308e1a2458084dd8
mherranzcruz2017 <36241957+mherranzcruz@users.noreply.github.com>
2018-05-21 16:29:35 +0200

    Programa 4
    2 tons la vegada

---
+++ Programa_4/Programa_4.ino
@@ -0,0 +1,39 @@
+/*****************************************************************
+**                                                              **
+**                        Títol:xiulet durant 3 segons                                **
+**          Posar nom representatiu del programa                **
+**                                                              **
+** NOM:Martin Herranz                          Data:21/05/2018  **
+*****************************************************************/
+//****************************INCLUDE*****************************
+
+
+//***************************VARIABLES****************************
+const int xiulet = 9;        // donar nom al pin 9 de l’Arduino
+//*****************************SETUP******************************
+
+void setup() {                 //Configura el final de salida
+ pinMode(xiulet, OUTPUT);   // definir el pin 9 com una sortida
+ 
+
+}
+
+
+
+
+
+//******************************LOOP******************************
+
+void loop() {                 //Inicia el bucle del programa
+
+  tone(xiulet,1000,3000);
+  tone(xiulet,2000,3000);
+  
+  
+  
+ 
+  
+
+}
+
+//****************************FUNCIONS****************************
