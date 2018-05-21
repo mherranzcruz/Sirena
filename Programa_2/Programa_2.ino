a0bdf74d55ff5ccf41d4c6a0327fe43d9175e1e1
mherranzcruz2017 <36241957+mherranzcruz@users.noreply.github.com>
2018-05-21 16:07:50 +0200

    Programa 2
    Xiulet durant 2 segons

---
+++ Programa_2/Programa_2.ino
@@ -0,0 +1,36 @@
+/*****************************************************************
+**                                                              **
+**                        Títol:                                **
+**          Posar nom representatiu del programa                **
+**                                                              **
+** NOM:Martin Herranz                          Data:21/05/2018  **
+*****************************************************************/
+//****************************INCLUDE*****************************
+
+
+//***************************VARIABLES****************************
+const int xiulet = 9;        // donar nom al pin 9 de l’Arduino
+//*****************************SETUP******************************
+
+void setup() {                 //Configura el final de salida
+ pinMode(xiulet, OUTPUT);   // definir el pin 9 com una sortida
+
+  tone(xiulet, 1000,3000);    // 
+}
+
+
+
+
+
+//******************************LOOP******************************
+
+void loop() {                 //Inicia el bucle del programa
+
+
+
+
+  
+
+}
+
+//****************************FUNCIONS****************************
